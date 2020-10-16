1512901353b0f941efd421fba78bb8de66fa2097
Matthias Dittgen <dittgen@gmail.com>
2020-10-16 23:05:56 +0200

    Cleaner ES6 in tests

--- test/unit/lib/filters/host.test.js
+++ test/unit/lib/filters/host.test.js
@@ -1,24 +1,22 @@
-var tap = require('tap');
-var httpMocks = require('node-mocks-http');
-var hostnameFilter = require('../../../../lib/filters/host');
+const tap = require('tap');
+const httpMocks = require('node-mocks-http');
+const hostnameFilter = require('../../../../lib/filters/host');
 
-tap.test('host filter module', function (tap) {
+tap.test('host filter module', tap => {
   
-  tap.beforeEach(function (done) {
-    done();
-  });
-
-  tap.test('when created', function (tap) {
+  tap.test('when created', tap => {
     tap.plan(2);
+
     tap.false(hostnameFilter(), 'should return false when no hostname is specified');
     tap.strictEqual(typeof hostnameFilter('test-a.com'), 'function', 'should return a function');
+
     tap.end();
   });
 
-  tap.test('when using with differnet hostnames as a filter function', function (tap) {
+  tap.test('when using with differnet hostnames as a filter function', tap => {
     tap.plan(2);
 
-    var req = httpMocks.createRequest({
+    const req = httpMocks.createRequest({
       method: 'GET',
       url: '/',
       headers: {
@@ -29,14 +27,15 @@ tap.test('host filter module', function (tap) {
 
     tap.true(hostnameFilter('test-a.com')(req), 'should match hostname');
     tap.false(hostnameFilter('test-b.com')(req), 'should not match hostname');
+
     tap.end();
   });
 
 
-  tap.test('when using but no there is no hostname in header', function (tap) {
+  tap.test('when using but no there is no hostname in header', tap => {
     tap.plan(1);
 
-    var req = httpMocks.createRequest({
+    const req = httpMocks.createRequest({
       method: 'GET',
       url: '/',
       headers: {},
@@ -44,14 +43,9 @@ tap.test('host filter module', function (tap) {
     });
 
     tap.false(hostnameFilter('test-a.com')(req), 'should not match hostname');
+
     tap.end();
   });
 
-  tap.afterEach(function (done) {
-    req = null;
-    res = null;
-    done();
-  });
-
   tap.end();
-})
+});
--- test/unit/lib/filters/method.test.js
+++ test/unit/lib/filters/method.test.js
@@ -1,23 +1,25 @@
-var tap = require('tap');
-var httpMocks = require('node-mocks-http');
-var methodFilter = require('../../../../lib/filters/method');
+const tap = require('tap');
+const httpMocks = require('node-mocks-http');
+const methodFilter = require('../../../../lib/filters/method');
 
-tap.test('method filter module', function (tap) {
+tap.test('method filter module',  tap => {
   
-  tap.test('when created', function (tap) {
+  tap.test('when created', tap => {
     tap.plan(5);
+
     tap.false(methodFilter(), 'should return false when created without method');
     tap.false(methodFilter('UGLIFY'), 'should return false when created without invalid method');
     tap.strictEqual(typeof methodFilter('GET'), 'function', 'should return a function when created with valid one valid method');
     tap.strictEqual(typeof methodFilter(['GET', 'POST']), 'function', 'should return a function when created with an array of valid methods');
     tap.strictEqual(typeof methodFilter(['GET', 'UGLIFY']), 'function','should return a function when created without a single valid method');
+
     tap.end();
   });
 
-  tap.test('when using with different method names as filter function', function (tap) {
+  tap.test('when using with different method names as filter function', tap => {
     tap.plan(3);
 
-    var req = httpMocks.createRequest({
+    const req = httpMocks.createRequest({
       method: 'GET',
       url: '/',
     });
@@ -25,8 +27,9 @@ tap.test('method filter module', function (tap) {
     tap.true(methodFilter('GET')(req), 'should match single uppercase method');
     tap.true(methodFilter('Get')(req), 'should match single mixed case method');
     tap.true(methodFilter(['GET', 'POST'])(req), 'should match a method from list');
+
     tap.end();
   });
 
   tap.end();
-})
+});
