91422f1318cceccc343781867d5738abbfcfede2
Matthias Dittgen <dittgen@gmail.com>
2020-10-16 17:47:01 +0200

    Convert hostname filter to be used with filter middleware

--- test/unit/lib/filters/host.test.js
+++ test/unit/lib/filters/host.test.js
@@ -1,14 +1,25 @@
 var tap = require('tap');
 var sinon = require('sinon');
 var httpMocks = require('node-mocks-http');
+var hostnameFilter = require('../../../../lib/filters/host');
 
-var filterHost = require('../../../../lib/filters/host');
-
-tap.test('host middleware module', function (tap) {
-  var req, res;
+tap.test('host filter module', function (tap) {
   
   tap.beforeEach(function (done) {
-    req = httpMocks.createRequest({
+    done();
+  });
+
+  tap.test('when created', function (tap) {
+    tap.plan(2);
+    tap.false(hostnameFilter(), 'should return false when no hostname is specified');
+    tap.strictEqual(typeof hostnameFilter('test-a.com'), 'function', 'should return a function');
+    tap.end();
+  });
+
+  tap.test('when using with differnet hostnames as a filter function', function (tap) {
+    tap.plan(2);
+
+    var req = httpMocks.createRequest({
       method: 'GET',
       url: '/',
       headers: {
@@ -17,82 +28,23 @@ tap.test('host middleware module', function (tap) {
       params: {}
     });
 
-    res = httpMocks.createResponse({
-      eventEmitter: require('events').EventEmitter
-    });
-
-    done();
+    tap.true(hostnameFilter('test-a.com')(req), 'should match hostname');
+    tap.false(hostnameFilter('test-b.com')(req), 'should not match hostname');
+    tap.end();
   });
 
-  tap.test('when created without hostname', function (tap) {
+
+  tap.test('when using but no there is no hostname in header', function (tap) {
     tap.plan(1);
-    var filterHostMiddleware = filterHost();
 
-    tap.false(filterHostMiddleware, 'should return false');
-    tap.end();
-  });
-
-  tap.test('when created with hostname and middleware function', function (tap) {
-    tap.plan(2);
-    var filterHostMiddleware = filterHost('test-a.com', sinon.fake());
-
-    tap.strictEqual(typeof filterHostMiddleware, 'function', 'should return a middleware function');
-    tap.strictEqual(filterHostMiddleware.length, 3, 'that excepts three arguments by default');
-    tap.end();
-  });
-
-  tap.test('when returned middleware function is used with matching host', function (tap) {
-    tap.plan(4);
-    var nextSpy = sinon.fake();
-    var middlewareSpy = sinon.spy(function (req, res, next) {
-      next();
-    });
-
-    var filterHostMiddleware = filterHost('test-a.com', middlewareSpy);
-    filterHostMiddleware(req, res, nextSpy);
-
-    tap.ok(middlewareSpy.calledOnce, 'should call middleware once');
-    tap.ok(middlewareSpy.calledWith(req, res), 'should call middleware with req and res');
-    tap.strictEqual(typeof middlewareSpy.lastCall.lastArg, 'function', 'should call middleware with a next function');
-    tap.ok(nextSpy.calledOnce, 'should call next once after the middleware');
-    tap.end();
-  });
-
-  tap.test('when returned middleware function is used with non matching host', function (tap) {
-    tap.plan(2);
-    var nextSpy = sinon.fake();
-    var middlewareSpy = sinon.spy(function (req, res, next) {
-      next();
-    });
-
-    var filterHostMiddleware = filterHost('test-b.com', middlewareSpy);
-    filterHostMiddleware(req, res, nextSpy);
-
-    tap.ok(middlewareSpy.notCalled, 'should not call middleware once');
-    tap.ok(nextSpy.calledOnce, 'should call next once after the middleware');
-    tap.end();
-  });
-
-  tap.test('when returned middleware function is used with no host at all', function (tap) {
-    tap.plan(2);
-
-    req = httpMocks.createRequest({
+    var req = httpMocks.createRequest({
       method: 'GET',
       url: '/',
       headers: {},
       params: {}
     });
 
-    var nextSpy = sinon.fake();
-    var middlewareSpy = sinon.spy(function (req, res, next) {
-      next();
-    });
-
-    var filterHostMiddleware = filterHost('test-b.com', middlewareSpy);
-    filterHostMiddleware(req, res, nextSpy);
-
-    tap.ok(middlewareSpy.notCalled, 'should not call middleware once');
-    tap.ok(nextSpy.calledOnce, 'should call next once after the middleware');
+    tap.false(hostnameFilter('test-a.com')(req), 'should not match hostname');
     tap.end();
   });
 
