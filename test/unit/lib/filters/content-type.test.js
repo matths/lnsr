519607a4a99256772dc950f21895955ecf26e2c7
Matthias Dittgen <dittgen@gmail.com>
2020-10-23 18:14:51 +0200

    Add content type filter

---
+++ test/unit/lib/filters/content-type.test.js
@@ -0,0 +1,66 @@
+const tap = require('tap');
+const sinon = require('sinon');
+const httpMocks = require('node-mocks-http');
+const contentTypeFilter = require('../../../../lib/filters/content-type');
+
+tap.test('content-type filter module', tap => {
+  
+  tap.test('when created', tap => {
+    tap.plan(2);
+
+    tap.false(contentTypeFilter(), 'should return false when no content-type is specified');
+    tap.strictEqual(typeof contentTypeFilter('text/plain'), 'function', 'should return a function');
+
+    tap.end();
+  });
+
+  tap.test('when using with a content-type as a filter function', tap => {
+    tap.plan(2);
+
+    const req = httpMocks.createRequest({
+      method: 'GET',
+      url: '/',
+      headers: {
+          'content-type': 'text/plain'
+      }
+    });
+
+    tap.true(contentTypeFilter('text/plain')(req), 'should match content-type');
+    tap.false(contentTypeFilter('text/html')(req), 'should not match content-type');
+
+    tap.end();
+  });
+
+  tap.test('when using with multiple content-types as a filter function', tap => {
+    tap.plan(2);
+
+    const req = httpMocks.createRequest({
+      method: 'GET',
+      url: '/',
+      headers: {
+          'content-type': 'text/plain'
+      }
+    });
+
+    tap.true(contentTypeFilter(['text/plain', 'text/html'])(req), 'should match content-type');
+    tap.false(contentTypeFilter(['application/json', 'text/javascript'])(req), 'should not match content-type');
+
+    tap.end();
+  });
+
+  tap.test('when client is not sending content-type header', tap => {
+    tap.plan(2);
+
+    const req = httpMocks.createRequest({
+      method: 'GET',
+      url: '/'
+    });
+
+    tap.false(contentTypeFilter('text/plain')(req), 'should not match content-type');
+    tap.false(contentTypeFilter(['application/json', 'text/javascript'])(req), 'should not match content-type');
+
+    tap.end();
+  });
+
+  tap.end();
+})
