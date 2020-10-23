470135ae7cf9e8c75851e3d6c8fb91af05b8b86c
Matthias Dittgen <dittgen@gmail.com>
2020-10-23 17:18:53 +0200

    Reduce method filter to single arg

--- test/unit/lib/filters/method.test.js
+++ test/unit/lib/filters/method.test.js
@@ -8,7 +8,7 @@ tap.test('method filter module',  tap => {
     tap.plan(5);
 
     tap.false(methodFilter(), 'should return false when created without method');
-    tap.false(methodFilter('UGLIFY'), 'should return false when created without invalid method');
+    tap.false(methodFilter('UGLIFY'), 'should return false when created without valid method');
     tap.strictEqual(typeof methodFilter('GET'), 'function', 'should return a function when created with valid one valid method');
     tap.strictEqual(typeof methodFilter(['GET', 'POST']), 'function', 'should return a function when created with an array of valid methods');
     tap.strictEqual(typeof methodFilter(['GET', 'UGLIFY']), 'function','should return a function when created without a single valid method');
