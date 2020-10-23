a607ec297c1d0be8d96fb95b8abfd2e3c100e796
Matthias Dittgen <dittgen@gmail.com>
2020-10-23 18:51:57 +0200

    Test block and pass

---
+++ test/unit/lib/filters/block.test.js
@@ -0,0 +1,38 @@
+const tap = require('tap');
+const rewire = require('rewire');
+const httpMocks = require('node-mocks-http');
+const blockFilter = require('../../../../lib/filters/block');
+
+tap.test('filter module', tap => {
+  let req;
+  
+  tap.beforeEach(done => {
+    req = httpMocks.createRequest({
+      method: 'GET',
+      url: '/user/Bob',
+    });
+    done();
+  });
+
+  tap.test('when created blockFilter filter', tap => {
+    tap.plan(1);
+    const filter = blockFilter('Bob');
+    filter(req);
+    tap.true(typeof filter === "function", 'should return a function');
+    tap.end();
+  });
+
+  tap.test('when running blockFilter filter', tap => {
+    tap.plan(2);
+    tap.false(blockFilter('Alice')(req), 'should return false with parameters');
+    tap.false(blockFilter()(req), 'should return false without parameters');
+    tap.end();
+  });
+
+  tap.afterEach(done => {
+    req = null;
+    done();
+  });
+
+  tap.end();
+})
---
+++ test/unit/lib/filters/pass.test.js
@@ -0,0 +1,38 @@
+const tap = require('tap');
+const rewire = require('rewire');
+const httpMocks = require('node-mocks-http');
+const passFilter = require('../../../../lib/filters/pass');
+
+tap.test('filter module', tap => {
+  let req;
+  
+  tap.beforeEach(done => {
+    req = httpMocks.createRequest({
+      method: 'GET',
+      url: '/user/Bob',
+    });
+    done();
+  });
+
+  tap.test('when created passFilter filter', tap => {
+    tap.plan(1);
+    const filter = passFilter('Bob');
+    filter(req);
+    tap.true(typeof filter === "function", 'should return a function');
+    tap.end();
+  });
+
+  tap.test('when running passFilter filter', tap => {
+    tap.plan(2);
+    tap.true(passFilter('Alice')(req), 'should return true with parameters');
+    tap.true(passFilter()(req), 'should return true without parameters');
+    tap.end();
+  });
+
+  tap.afterEach(done => {
+    req = null;
+    done();
+  });
+
+  tap.end();
+})
