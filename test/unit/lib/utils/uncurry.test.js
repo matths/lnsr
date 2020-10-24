fea8e5344d32814107a63208ff06d3d4e1af8d01
Matthias Dittgen <dittgen@gmail.com>
2020-10-24 09:46:01 +0200

    Add some more fp helpers

---
+++ test/unit/lib/utils/uncurry.test.js
@@ -0,0 +1,16 @@
+const tap = require('tap');
+const uncurry = require('../../../../lib/utils/uncurry');
+
+tap.test('uncurry', tap => {
+  let req;
+  
+  tap.test('when uncurried functions are used', tap => {
+    tap.plan(1);
+    const sum = a => b => a + b;
+    const uncurriedSum = uncurry(sum);
+    tap.strictEqual(sum(1)(2), uncurriedSum(1,2), 'should lead to same result for methods two parameters');
+    tap.end();
+  });
+
+  tap.end();
+})
