a38c69e5cc90a97861746913e784109b08d12cc5
Matthias Dittgen <dittgen@gmail.com>
2020-11-06 09:23:01 +0100

    Add basic authorization

---
+++ test/unit/lib/interact/authorization.test.mjs
@@ -0,0 +1,69 @@
+import { EventEmitter } from 'events';
+import tap from 'tap';
+import sinon from 'sinon';
+import httpMocks from 'node-mocks-http';
+import authorizationMiddleware from '../../../../lib/interact/authorization.mjs';
+
+tap.test('authoriztion middleware module', tap => {
+
+  tap.test('when created', tap => {
+    tap.plan(1);
+    const middlewareSpy = sinon.spy((req, res, next) => {
+      next();
+    });
+    tap.strictEqual(typeof authorizationMiddleware({}, middlewareSpy), 'function', 'should return a middleware function');
+    tap.end();
+  });
+
+  tap.test('when running without a authorization header', tap => {
+    tap.plan(3);
+
+    const req = httpMocks.createRequest({
+      method: 'GET',
+      url: '/user/Bob',
+    });
+    const res = httpMocks.createResponse({
+      eventEmitter: EventEmitter
+    });
+    res.on('finish', () => {
+      tap.strictEqual(res.statusCode, 401, 'returns 401');
+    });
+
+    const nextSpy = sinon.fake();
+    const middlewareSpy = sinon.spy((req, res, next) => {
+      next();
+    });
+
+    authorizationMiddleware({users: { agent007: "top-secret" }, realm: "Keep out area!"}, middlewareSpy)(req, res, nextSpy);
+    tap.strictEqual(middlewareSpy.callCount, 0, 'should not call middleware');
+    tap.strictEqual(nextSpy.callCount, 0, 'should not call next');
+    tap.end();
+  });
+
+  tap.test('when running with a valid authorization header', tap => {
+    tap.plan(2);
+
+    const req = httpMocks.createRequest({
+      method: 'GET',
+      url: '/user/Bob',
+      headers: {
+        authorization: 'Basic YWdlbnQwMDc6dG9wLXNlY3JldA=='
+      }
+    });
+    const res = httpMocks.createResponse({
+      eventEmitter: EventEmitter
+    });
+
+    const nextSpy = sinon.fake();
+    const middlewareSpy = sinon.spy((req, res, next) => {
+      next();
+    });
+
+    authorizationMiddleware({users: { agent007: "top-secret" }, realm: "Keep out area!"}, middlewareSpy)(req, res, nextSpy);
+    tap.true(middlewareSpy.calledOnce, 'should call middleware once');
+    tap.true(nextSpy.calledOnce, 'should call next');
+    tap.end();
+  });
+
+  tap.end();
+});
---
+++ test/unit/lib/utils/base64.test.mjs
@@ -0,0 +1,19 @@
+import tap from 'tap';
+import {atob, btoa} from '../../../../lib/utils/base64.mjs';
+
+tap.test('base64', tap => {
+
+  tap.test('when using atob', tap => {
+    tap.plan(1);
+    tap.strictEqual(atob('aGVsbG8gd29ybGQ='), 'hello world', 'should encode ascii string as base64');
+    tap.end();
+  });
+
+  tap.test('when using btoa', tap => {
+    tap.plan(1);
+    tap.strictEqual(btoa('hello world'), 'aGVsbG8gd29ybGQ=', 'should decode base64 to ascii string');
+    tap.end();
+  });
+
+  tap.end();
+})
