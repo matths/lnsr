c67bc3e19d6123d893178894c646ae4fd09a2488
Matthias Dittgen <dittgen@gmail.com>
2020-11-06 10:11:48 +0100

    Add nother test for authorization

--- test/unit/lib/interact/authorization.test.mjs
+++ test/unit/lib/interact/authorization.test.mjs
@@ -40,6 +40,34 @@ tap.test('authoriztion middleware module', tap => {
     tap.end();
   });
 
+  tap.test('when running with a authorization header but existing user but wrong password', tap => {
+    tap.plan(3);
+
+    const req = httpMocks.createRequest({
+      method: 'GET',
+      url: '/user/Bob',
+      headers: {
+        authorization: 'Basic YWdlbnQwMDc6bGV0LW1lLWluLXBsZWFzZQ=='
+      }
+    });
+    const res = httpMocks.createResponse({
+      eventEmitter: EventEmitter
+    });
+    res.on('finish', () => {
+      tap.strictEqual(res.statusCode, 401, 'returns 401');
+    });
+
+    const nextSpy = sinon.fake();
+    const middlewareSpy = sinon.spy((req, res, next) => {
+      next();
+    });
+
+    authorizationMiddleware({users: { agent007: "top-secret" }, realm: "Keep out area!"}, middlewareSpy)(req, res, nextSpy);
+    tap.strictEqual(middlewareSpy.callCount, 0, 'should not call middleware');
+    tap.strictEqual(nextSpy.callCount, 0, 'should not call next');
+    tap.end();
+  });
+
   tap.test('when running with a valid authorization header', tap => {
     tap.plan(2);
 
